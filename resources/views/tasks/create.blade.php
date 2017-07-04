6f30fa9d75604a60651f80b165eb078eebdf08cd
mymnsok <ma051778@gmail.com>
2017-07-04 00:36:04 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,18 +5,25 @@
     <h1>タスク新規作成ページ</h1>
     
     @include('commons.error_messages')
-    
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
-        {!! Form::label('content' , 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-        
-        {!! Form::label('status' , 'ステータス:') !!}
-        {!! Form::select('status', ['Done' =>'Done', 'Not yet' => 'Not yet'], 'Not yet') !!}
-        
-        {!! Form::submit('投稿') !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-5 col-lg-offset-3 col-sm-6">    
+        {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-    {!! Form::close() !!}
+            <div class="form-group">
+                {!! Form::label('status' , 'ステータス:') !!}
+                {!! Form::select('status', ['Done' =>'Done', 'Not yet' => 'Not yet'], 'Not yet', ['class' => 'form-control']) !!}
+            </div>
 
+            <div class="form-group">
+                {!! Form::label('content' , 'タスク:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+       
+            {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+        {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,18 +5,24 @@
 <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
    @include('commons.error_messages')
-   
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+
+    <div class="row">
+        <div class="col-xs-6">     
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
     
+                <div class="form-group">                
+                    {!! Form::label('status' , 'ステータス:') !!}
+                    {!! Form::select('status', ['Done' =>'Done', 'Not yet' => 'Not yet'], 'Not yet', ['class' => 'form-control']) !!}
+                </div>
 
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-        
-        {!! Form::label('status' , 'ステータス:') !!}
-        {!! Form::select('status', ['Done' =>'Done', 'Not yet' => 'Not yet'], 'Not yet') !!}
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
 
-        {!! Form::submit('更新') !!}
+                {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
 
-    {!! Form::close() !!}
-
+            {!! Form::close() !!}
+        </div>
+    </div>
 @endsection
