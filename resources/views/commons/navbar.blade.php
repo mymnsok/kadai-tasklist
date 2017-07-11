8b23aaa574b839ff953e7ddda6da8bcf82169ea0
mymnsok <ma051778@gmail.com>
2017-07-11 21:12:06 +0000

    login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,10 +10,17 @@
       <a class="navbar-brand" href="/">Tasklist</a>
     </div>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-       <ul class="nav navbar-nav navbar-right">
-        <li>{!! link_to_route('tasks.create' , '新規タスク作成') !!}</li>
-       </ul>
-   </div>
-  </div><!-- /.container-fluid -->
-</nav>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    @if (Auth::check())
+                        <li>{!! link_to_route('tasks.create' , '新規タスク作成') !!}</li>
+                        <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                    @else
+                        <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                        <li>{!! link_to_route('login.get', 'Login') !!}</li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <body>
         @include('commons.navbar')
         <div class="container">
+            @include('commons.error_messages')
             @yield('content')
         </div>
     </body>
